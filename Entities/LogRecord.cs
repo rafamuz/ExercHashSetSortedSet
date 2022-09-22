67d66bbe10c91947daf70dc7ffc7e9357db94ba2
Rafael Muzel <rafaelmuzel@yahoo.com.br>
2022-09-22 21:40:32 +0100

    Adicionar arquivos de projeto.

---
+++ Entities/LogRecord.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercHashSetSortedSet.Entities
+{
+    internal class LogRecord
+    {
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Username.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is LogRecord))
+            {
+                return false;
+            }
+            LogRecord other = obj as LogRecord;
+            return Username.Equals(other.Username);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using ExercHashSetSortedSet.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercHashSetSortedSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Read a file with users log.
+            Console.Write("Enter the file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                HashSet<LogRecord> logRecords = new HashSet<LogRecord>();
+                using (StreamReader sr = File.OpenText(path)) {
+                    while(!sr.EndOfStream)
+                    {                        
+                        string[] vectLine = sr.ReadLine().Split(' ');
+                        //Gets only the user from the line that was read.
+                        string username = vectLine[0];
+                        DateTime instant = DateTime.Parse(vectLine[1]);                        
+                        logRecords.Add(new LogRecord() { Username = username, Instant = instant });                        
+                    }
+                }
+                Console.WriteLine("Total users: " + logRecords.Count);
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("Error during file access: " + e.Message);
+            }
+        }
+    }
+}
